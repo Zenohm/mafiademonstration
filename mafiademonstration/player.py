82bf53e9b0af43edd52e275ed8121ab7f878d02d
Zenohm <sentherus@gmail.com>
2017-06-06 16:54:08 -0400

    Refactoring Update Moved a few classes around, preparing to switch over to menu setup The Player class and its associated kivy code have been moved into their own file. The stages file now has the code for the discussion stage as well as the generic Stage object set up. All Screens now inherit from that Stage object as well. Run the stages.py file from the project root directory to see what the new menus look like.

---
+++ mafiademonstration/player.py
@@ -0,0 +1,88 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import (
+        BooleanProperty, BoundedNumericProperty, DictProperty,
+        ObjectProperty, StringProperty, ListProperty, NumericProperty
+)
+from kivy.logger import Logger
+
+try:
+    from mafiademonstration.border_behavior import BorderBehavior
+except ModuleNotFoundError:
+    from border_behavior import BorderBehavior
+
+
+
+Builder.load_string("""
+#:import to_rgba kivy.utils.get_color_from_hex
+
+<Player>:
+    name: "player"
+    number: 0
+    icon: "./data/icons/player_alive.png"
+    alive: True
+    on_alive: root.ready_action("die")
+    mafia: False
+    strategic_value: 0
+    current_action: "accuse"
+    # on_current_action: self.borders = (2, "solid", to_rgba("05F5F5")); self.update_borders()
+    actions: {"accuse": {"player": None, "reason": None}, "suspect": {"player": None, "reason": None}, "kill": None, "vote": "abstain"}
+    borders: 2, "solid", to_rgba("F5F5F5")
+
+    orientation: "vertical"
+    canvas.before:
+        Color:
+            rgba: to_rgba("F5F5F5")
+        Rectangle:
+            # self here refers to the widget i.e BoxLayout
+            pos: self.pos
+            size: self.size
+    ImageButton:
+        source: root.icon
+        on_press: app.root.selected_player.act_on(root) if app.root.selected_player else None
+    Spinner:
+        text: "Action"
+        color: to_rgba("F5F5F5")
+        background_color: to_rgba("212121")
+        values: "Accuse", "Suspect", "Clear"
+        on_text: app.root.selected_player = root.ready_action(self.text)
+""")
+
+class Player(BoxLayout, BorderBehavior):
+    name = StringProperty("")
+    number = NumericProperty(0)
+    alive = BooleanProperty(True)
+    mafia = BooleanProperty(False)
+    strategic_value = NumericProperty(0)
+    current_action = StringProperty()  # Holds a reference to the action that is to be taken on another player.
+    actions = DictProperty()
+
+    def ready_action(self, action):
+        """ Designate the current player as the one who will be performing actions.
+            This is done by setting the self player instance as the selected player.
+        """
+        self.current_action = action.lower()
+        Logger.info("{name} is ready to {action}".format(name=self.name,
+                                                         action=self.current_action))
+
+        if self.current_action == "clear":
+            self.actions = {"accuse": None, "suspect": None,
+                            "kill": None, "vote": None}
+            self.alive = False
+        if self.current_action == "die":
+            self.actions = {}
+            self.alive = False
+            self.icon = "./data/icons/player_dead.png"
+        return self
+
+    def act_on(self, player):
+        assert isinstance(player, type(self))
+
+        if self == player:
+            # TODO: Figure out how I want to handle this.
+            return
+
+        self.actions[self.current_action] = player
+        Logger.info("{self} {action} {other}".format(self=self.name, action=self.current_action, other=player.name))
+        return self
+
--- mafiademonstration/stages.py
+++ mafiademonstration/stages.py
@@ -3,6 +3,28 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+
+from kivy.garden.circularlayout import CircularLayout
+from kivy.garden.modernmenu import ModernMenu
+from kivy.logger import Logger
+from kivy.properties import (
+    BooleanProperty, BoundedNumericProperty, DictProperty,
+    ObjectProperty, StringProperty, ListProperty, NumericProperty
+)
+
+
+try:
+    from mafiademonstration.border_behavior import BorderBehavior
+    from mafiademonstration.player import Player
+except ModuleNotFoundError:
+    from border_behavior import BorderBehavior
+    from player import Player
+
 
 Builder.load_string("""
 #:import to_rgba kivy.utils.get_color_from_hex
@@ -21,10 +43,10 @@ Builder.load_string("""
 
         Label:
             color: to_rgba("212121")
-            text: 'Mafia Game Logo'
+            text: "Mafia Game Logo"
 
         BoxLayout:
-            orientation: 'horizontal'
+            orientation: "horizontal"
             cols: 3
 
             Label:
@@ -38,34 +60,34 @@ Builder.load_string("""
                 Label:
 
                 Button:
-                    text: 'Start'
-                    on_press: root.manager.current = 'loading'
+                    text: "Start"
+                    on_press: root.manager.current = "loading"
 
                 Button:
-                    text: 'Settings'
-                    on_press: root.manager.current = 'settings'
+                    text: "Settings"
+                    on_press: root.manager.current = "settings"
 
                 Button:
-                    text: 'Exit'
+                    text: "Exit"
                     on_press: exit()
 
             Label:
 
 # <SettingsItem>
-#     text: ''
+#     text: ""
 #
 #     Label:
 #         id: item_info
 #         color: to_rgba("212121")
 #         # size_hint_x: None
-#         anchor_x: 'left'
+#         anchor_x: "left"
 #         text: root.text
 #
 #     Button:
 #         id: item_input
 #         size_hint_x: 0.3
-#         anchor_x: 'right'
-#         text: 'testing'
+#         anchor_x: "right"
+#         text: "testing"
 #
 
 
@@ -79,16 +101,16 @@ Builder.load_string("""
             size: self.size
 
     BoxLayout:
-        orientation: 'vertical'
+        orientation: "vertical"
         padding: [20, 25]
 
         Label:
             size_hint_x: None
             size_hint_y: 0.1
             color: to_rgba("212121")
-            halign: 'left'
+            halign: "left"
             markup: True
-            text: '[b]Settings[/b]'
+            text: "[b]Settings[/b]"
 
         GridLayout:
             cols: 2
@@ -105,61 +127,61 @@ Builder.load_string("""
             Label:
                 size_hint_x: None
                 color: to_rgba("212121")
-                halign: 'left'
-                text: 'testing'
+                halign: "left"
+                text: "testing"
 
             TextInput:
                 size_hint_x: 2
-                text: 'testing'
+                text: "testing"
 
             Label:
                 size_hint_x: None
                 color: to_rgba("212121")
-                halign: 'left'
-                text: 'testing'
+                halign: "left"
+                text: "testing"
 
             TextInput:
                 size_hint_x: 2
-                text: 'testing'
+                text: "testing"
 
             Label:
                 size_hint_x: None
                 color: to_rgba("212121")
-                halign: 'left'
-                text: 'testing'
+                halign: "left"
+                text: "testing"
 
             TextInput:
                 size_hint_x: 2
-                text: 'testing'
+                text: "testing"
 
             Label:
                 size_hint_x: None
                 color: to_rgba("212121")
-                halign: 'left'
-                text: 'testing'
+                halign: "left"
+                text: "testing"
 
             TextInput:
                 size_hint_x: 2
-                text: 'testing'
+                text: "testing"
 
             Label:
                 size_hint_x: None
                 color: to_rgba("212121")
-                halign: 'left'
-                text: 'testing'
+                halign: "left"
+                text: "testing"
 
             TextInput:
                 size_hint_x: 2
-                text: 'testing'
+                text: "testing"
 
         Label:
             size_hint_y: 0.1
 
         Button:
             size_hint: (0.35, 0.15)
-            pos_hint: {'center_x': 0.825}
-            text: 'Return'
-            on_press: root.manager.current = 'menu'
+            pos_hint: {"center_x": 0.825}
+            text: "Return"
+            on_press: root.manager.current = "menu"
 
 
 <LoadingScreen>:
@@ -174,39 +196,119 @@ Builder.load_string("""
     BoxLayout:
         Label:
             color: to_rgba("212121")
-            text: 'Loading...'
+            text: "Loading..."
+        Button:
+            text: "Next Screen"
+            on_press: root.manager.current = "discussion"
+
+<DiscussionScreen>:
+    AnchorLayout:
+        players: {}
+        selected_player: None
+        ready_to_submit: False
+        on_ready_to_submit: submit_button = "./data/icons/submit_complete.png" if ready_to_submit else "./data/icons/submit_incomplete.png"
+        canvas.before:
+            Color:
+                rgba: to_rgba("F5F5F5")
+            Rectangle:
+                pos: self.pos
+                size: self.size
+
+        ImageButton:
+            id: submit_button
+            source: "./data/icons/submit_incomplete.png"
+            color: to_rgba("05F5F5")
+            on_press: app.submit()
+            size_hint: .2, .2
+            borders: 2, "solid", (1,1,1,1.)
+
+        CircularLayout:
+            id: circular_layout
+            direction: "ccw"
+            inner_radius_hint: 1
+            outer_radius_hint: 1.5
+            size_hint_x: 0.6
+            size_hint_y: 0.5
+            start_angle: 60
 
 """)
 
+class Stage(Screen):
+    selected_player = ObjectProperty()
+    players = ListProperty()
+    next_stage = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(Stage, self).__init__(**kwargs)
+
+    def enter(self, **kwargs):
+        pass
+
+    def exit(self, **kwargs):
+        pass
 
 # class SettingsItem(AnchorLayout):
 #     pass
 
-class MenuScreen(Screen):
+class MenuScreen(Stage):
     pass
 
 
-class SettingsScreen(Screen):
+class SettingsScreen(Stage):
     pass
 
 
-class LoadingScreen(Screen):
+class LoadingScreen(Stage):
     pass
 
 
-class DiscussionScreen(Screen):
+class DiscussionScreen(Stage):
+    def __init__(self, **kwargs):
+        super(DiscussionScreen, self).__init__(**kwargs)
+        self.player_count = 6 #int(self.config.get('user_settings', 'player_count'))
+        self.agent_number = 0 #int(self.config.get('user_settings', 'agent_number'))
+        # self.language = self.config.get('user_settings', 'language')
+
+        # user_interval = self.config.get('user_settings', 'timer_interval')
+        # self.timer_interval = TIMER_OPTIONS[user_interval]
+
+        players = dict()
+        for player_number in range(1, self.player_count + 1):
+            player_name = 'player {}'.format(player_number)
+            player = Player(name=player_name)
+            player.number = player_number
+            # player.borders = (2, "solid",(0,2,3,4))
+
+            if player_number == self.agent_number:
+                # All values that need to be set for the agent
+                # should be set here.
+                player.icon = './data/icons/agent.png'
+
+            players[player_name] = player
+            self.ids.circular_layout.add_widget(player)
+
+        Stage.players = players
+        # This will be used to keep track of who is acting against whom.
+        Stage.selected_player = None
+
+
+class TrialScreen(Stage):
     pass
 
 
-class TrialScreen(Screen):
+class NightScreen(Stage):
     pass
 
 
-class NightScreen(Screen):
+class EndGameScreen(Stage):
     pass
 
 
-class EndGameScreen(Screen):
+class ImageButton(BorderBehavior, ButtonBehavior, Image):
+    pass
+
+
+class ActionList(DropDown):
     pass
 
 
